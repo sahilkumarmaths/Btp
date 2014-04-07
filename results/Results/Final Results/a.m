3f2efc233ea33a99c278e217a96593eea5ef848c
Sahil Kumar <sahilkumarmaths@gmail.com>
2014-04-07 15:51:13 +0530

    graphs modified

--- results/Results/Final Results/a.m
+++ results/Results/Final Results/a.m
@@ -2,12 +2,12 @@ clear;
 load data1.txt
 figure;
 x = data1(1:20,2);
-z = plot(x,data1(1:20,5),x,data1(1:20,6),x,data1(1:20,7),x,data1(1:20,8),x,data1(1:20,9));
+z = plot(x,data1(1:20,5),"rx-",x,data1(1:20,6),"g*-",x,data1(1:20,7),"bo-",x,data1(1:20,8),"md-",x,data1(1:20,9),"cp-");
 title("Variation of entropy of binding with registers");
 xlabel("Registers");
 ylabel("Entropy");
 legend("Initial Value",	"Random Value",	"Left Right Value",	"Center Center Value","Blossom Value",'location', 'east');
-set (z,"linewidth", 2) ;
+set (z,"linewidth", 3) ;
 axis([5 110 0 1800]);
 print -deps -color data1.eps
 
@@ -15,12 +15,12 @@ clear;
 load data2.txt
 figure;
 x = data2(1:20,3);
-z = plot(x,data2(1:20,5),x,data2(1:20,6),x,data2(1:20,7),x,data2(1:20,8),x,data2(1:20,9));
+z = plot(x,data2(1:20,5),"rx-",x,data2(1:20,6),"g*-",x,data2(1:20,7),"bo-",x,data2(1:20,8),"md-",x,data2(1:20,9),"cp-");
 title("Variation of entropy of binding with phases");
 xlabel("Phases");
 ylabel("Entropy");
 legend("Initial Value",	"Random Value",	"Left Right Value",	"Center Center Value","Blossom Value",'location', 'east');
-set (z,"linewidth", 2) ;
+set (z,"linewidth", 3) ;
 axis([0 45 0 1800]);
 print -deps -color data2.eps
 
@@ -28,12 +28,12 @@ clear;
 load data3.txt
 figure;
 x = data3(1:20,4);
-z = plot(x,data3(1:20,5),x,data3(1:20,6),x,data3(1:20,7),x,data3(1:20,8),x,data3(1:20,9));
+z = plot(x,data3(1:20,5),"rx-",x,data3(1:20,6),"g*-",x,data3(1:20,7),"bo-",x,data3(1:20,8),"md-",x,data3(1:20,9),"cp-");
 title("Variation of entropy of binding with applications");
 xlabel("Applications");
 ylabel("Entropy");
 legend("Initial Value",	"Random Value",	"Left Right Value",	"Center Center Value","Blossom Value",'location', 'east');
-set (z,"linewidth", 2) ;
+set (z,"linewidth", 3) ;
 axis([2 25 0 600]);
 print -deps -color data3.eps
 
